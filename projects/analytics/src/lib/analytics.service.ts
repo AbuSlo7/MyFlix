0a6855e280e0186df3a06b1198d590cfdf82867b
saalajmi <saalajmi@tvtc.gov.sa>
2020-02-07 10:59:32 +0000

    11

--- projects/analytics/src/lib/analytics.service.ts
+++ projects/analytics/src/lib/analytics.service.ts
@@ -10,15 +10,15 @@ export class AnalyticsService {
 
   constructor(private http: HttpClient) { }
   getMoviesById(body: FormData): Observable<string[]> {
-    return this.http.post<string[]>('http://35.239.117.138:2006/api/analytics_selectMovieID.php', body);
+    return this.http.post<string[]>('http://analytics-app/api/analytics_selectMovieID.php', body);
   }
   getMovieIDs(): Observable<Movies[]> {
-    return this.http.get<Movies[]>('http://35.239.117.138:2006/api/getMovieID.php');
+    return this.http.get<Movies[]>('http://analytics-app/api/getMovieID.php');
   }
   saveUserData(body: FormData): Observable<string> {
-    return this.http.post<string>('http://35.239.117.138:2006/api/analyticsSaveUser.php', body);
+    return this.http.post<string>('http://analytics-app/api/analyticsSaveUser.php', body);
   }
   getEstimates(body: FormData): Observable<string[]> {
-    return this.http.post<string[]>('http://35.239.117.138:2006/api/getEstimates.php', body);
+    return this.http.post<string[]>('http://analytics-app/api/getEstimates.php', body);
   }
 }
